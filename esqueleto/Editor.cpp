bc54f5e0401094f4b6a83d67babc589afa117651
CatalinaDolhare <cdolhare@udesa.edu.ar>
2023-06-20 11:26:29 -0300

    Arreglo de complejidad

--- esqueleto/Editor.cpp
+++ esqueleto/Editor.cpp
@@ -5,7 +5,7 @@ using namespace std;
 using std::string;
 
 Editor::Editor(const set<string> & conectivos) {
-    _conectivos = conectivos; //O(|conectivos|)
+    _conectivos = conectivos; //O(|conectivos|) o O(1) PREGUNTAR
     _texto = {}; //O(1)
     _vocabulario = {}; //O(1)
     _cantidad_palabras = 0; //O(1)
@@ -18,16 +18,15 @@ Editor::Editor(const set<string> & conectivos) {
 
 string Editor::texto() const {
     string texto_completo = ""; //O(1)
-    for(int i=0; i<_texto.size()-1; i++){ //O(1), |_texto|
+    for(int i=0; i<_texto.size()-1; i++){ //O(1), N iteraciones
         texto_completo.append(_texto[i]); //O(1)
         texto_completo.append(" "); //O(1)
     }
     texto_completo.append(_texto[_texto.size()-1]); //O(1)
     return texto_completo; //O(1)
 }
-//N = |_texto|
-//Complejidad = O(1) + n * (O(1) + O(1) + O(1)) + O(1) + O(1)
-//            = O(1) + n * ((1) + O(1)
+//Complejidad = O(1) + N * (O(1) + O(1) + O(1)) + O(1) + O(1)
+//            = O(1) + N * ((1) + O(1)
 //            = O(max{O(1), N, O(1)})
 //            = O(N)
 
@@ -85,17 +84,20 @@ void Editor::agregar_atras(const string& oracion) {
             _vocabulario.insert(oracion_separada[i]); //O(log M)
         }
         if(_posiciones_palabras.count(oracion_separada[i]) == 1){ //O(1)
-            _posiciones_palabras[oracion_separada[i]].insert(_longitud-1); //O(log M)
+            _posiciones_palabras[oracion_separada[i]].insert(_longitud-1); //O(log P)
         } else {
-            _posiciones_palabras[oracion_separada[i]] = {_longitud-1}; //O(log M)
+            _posiciones_palabras[oracion_separada[i]] = {_longitud-1}; //O(log P)
         }
     }
 }
-//Complejidad = O(1) + O(1) + |oracion| * (O(1) + O(1) + O(1) + O(log M) + O(1) + O(log M) + O(1) + O(log P) + O(1) + O(1))
-//            = O(1) + |oracion| * O(max{1, log M, log P}) 
-//            = O(1) + |oracion| * O(log M + log P) por regla de logaritmos, log M + log P = log M*P
-//            = O(1) + O(|oracion| * log (M*P))
-//            = O(max{1, |oracion| * log (M*P)})
+//Complejidad = O(1) + O(1) + O(|oracion|) + |oracion| * (O(1) + O(1) + O(log M) + O(1) + O(log M) + O(1) + O(log P) + O(log P))
+//            = O(1) + O(|oracion|) + |oracion| * O(max{1, log M, log P}) 
+//            = O(1) + O(|oracion|) + |oracion| * O(log M + log P) por regla de logaritmos, log M + log P = log M*P
+//            = O(1) + O(|oracion|) + O(|oracion| * log (M*P))
+//            = O(max{1, |oracion|, |oracion| * log (M*P)})
+//            = O(|oracion| + |oracion| * log (M*P))
+//            = O(|oracion|(1 + log M*P)) sacando factor comun |oracion|
+//            = O(|oracion| * max{1, log M*P})
 //            = O(|oracion| * log (M*P))
 
 const set<int> & Editor::buscar_palabra(const string& palabra) const {
@@ -159,8 +161,8 @@ void Editor::borrar_posicion(int pos) {
         _vocabulario.erase(palabra); //O(log M)
     }
 
-    for(int j=0; j<_longitud; j++){ //O(1), |_longitud| iteraciones
-        for(set<int>::iterator it = _posiciones_palabras[_texto[j]].begin(); it != _posiciones_palabras[_texto[j]].end(); it++){ //|_posiciones_palabras[_texto[j]]| iteraciones
+    for(int j=0; j<_longitud; j++){ //O(1), N iteraciones
+        for(set<int>::iterator it = _posiciones_palabras[_texto[j]].begin(); it != _posiciones_palabras[_texto[j]].end(); it++){ //|_posiciones_palabras[_texto[j]]| iteraciones -> creo que esto es P
             if(*it>pos){ //O(1)
                 int pos_nueva = *it-1; //O(1)
                 _posiciones_palabras[_texto[j]].erase(*it); //O(log M)
@@ -169,9 +171,15 @@ void Editor::borrar_posicion(int pos) {
         }
     }
 }
-//Complejidad = O(1) + O(log M) + O(1) + O(log M) + O(1) + O(N) + O(log M) + O(1) + O(log M) + O(log M) + |_longitud| * (O(1) + |_posiciones_palabras[_texto[j]]|*(O(1) + O(1) + O(log M) + O(log M)))
-//            = O(log M + N) + O(|_longitud|*|_posiciones_palabras[_texto[j]]|*log M)
-//            =O(log M + N + |_longitud|*|_posiciones_palabras[_texto[j]]|*log M)
+//Complejidad = O(1) + O(log M) + O(1) + O(log M) + O(1) + O(N) + O(log M) + O(1) + O(log M) + O(log M) + N * (O(1) + P*(O(1) + O(1) + O(log M) + O(log M)))
+//            = O(log M + N) + O(N*P*log M)
+//            = O(log M + N + N*P*log M)
+//            = O(log M + N*(1+P log M)) sacando factor comun N
+//            = O(log M + N*(max{1+P log M}))
+//            = O(log M + N P log M))
+//            = O(log M * (1 + N*P))) sacando factor comun log M
+//            = O(log M * (max{1 + N*P})))
+//            = O(log M * N * P)
 
 int Editor::borrar_palabra(const string& palabra) {
     auto it_encontrar_palabra = _posiciones_palabras.find(palabra);
